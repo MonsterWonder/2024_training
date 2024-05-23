04ce199529d21f547968b046745b07d8116793aa
syh <807968172@qq.com>
2024-05-23 21:32:01 +0800

    更新主文件

--- week01/vite_study/src/App.tsx
+++ week01/vite_study/src/App.tsx
@@ -1,10 +1,12 @@
-import Es6 from "./components/Es6";
+import Es6_2 from "./components/Es6_2";
+// import Fibonacci from "./components/Fibonacci";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Es6 />
+      {/* <Fibonacci></Fibonacci> */}
+      <Es6_2></Es6_2>
     </div>
   );
 }
