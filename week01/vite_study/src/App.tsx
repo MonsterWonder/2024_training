f0377a67e49467bbb6ec96eec9086648f605526f
syh <807968172@qq.com>
2024-06-03 21:43:08 +0800

    跨域代理配置

--- week01/vite_study/src/App.tsx
+++ week01/vite_study/src/App.tsx
@@ -1,14 +1,9 @@
-import Es6_3 from "./components/Es6_3";
-// import Fibonacci from "./components/Fibonacci";
 import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      {/* <Fibonacci></Fibonacci> */}
-      <Es6_3></Es6_3>
-    </div>
-  );
+export default class App extends Component {
+  getStudentData = () => {};
+  render() {
+    return <div></div>;
+  }
 }
-
-export default App;
