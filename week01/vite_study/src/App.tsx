14fe0eddb500fe08351bf2c6b0b762750a32431c
syh <807968172@qq.com>
2024-06-04 09:00:03 +0800

    重新组织代码库结构

--- week01/vite_study/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import "./App.css";
-import React, { Component } from "react";
-
-export default class App extends Component {
-  getStudentData = () => {};
-  render() {
-    return <div></div>;
-  }
-}
