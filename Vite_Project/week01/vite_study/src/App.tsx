6b2375d598b06aa96b89e8e26b4c734a504d3af0
syh <807968172@qq.com>
2024-06-04 08:50:02 +0800

    验证ES6特性的主文件

---
+++ Vite_Project/week01/vite_study/src/App.tsx
@@ -0,0 +1,9 @@
+import "./App.css";
+import React, { Component } from "react";
+
+export default class App extends Component {
+  getStudentData = () => {};
+  render() {
+    return <div></div>;
+  }
+}
