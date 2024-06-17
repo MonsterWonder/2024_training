484b77ae8454d907a44afe1f987fbd25781300fd
syh <807968172@qq.com>
2024-06-17 19:22:37 +0800

    删除单独的action文件，移动到actions文件夹

--- Vite_Project/week04/redux_test/src/redux/count_action.ts
+++
@@ -1,5 +0,0 @@
-// Action Creators 创建 action 对象
-import { INCREMENT, DECREMENT } from "./constants";
-
-export const incrementAction = (data) => ({ type: INCREMENT, data });
-export const decrementAction = (data) => ({ type: DECREMENT, data });
