36158cc92f701ef8ce42825fec2f0c7975bb8b41
syh <807968172@qq.com>
2024-06-17 19:24:18 +0800

    更新store文件

--- Vite_Project/week04/redux_test/src/redux/store.ts
+++ Vite_Project/week04/redux_test/src/redux/store.ts
@@ -1,5 +1,7 @@
 // 创建store
-import { createStore } from "redux";
-import countReducer from "./count_reducers";
+import { applyMiddleware, createStore } from "redux";
+// 引入redux-thunk，用于支持异步action
+import { thunk } from "redux-thunk";
 // 传入 reducer 参数
-export default createStore(countReducer);
+import reducers from "./reducers";
+export default createStore(reducers, applyMiddleware(thunk));
