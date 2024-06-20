a583eb41ff20a86aad3a5be4b39ad4fc04ba170f
syh <807968172@qq.com>
2024-06-20 09:15:47 +0800

    大作业中actions主文件，主要包含两个creators，add_todo用于包装新增待办的动作，toggle_todo主要包装切换完成状态的动作

---
+++ Vite_Project/src/redux/actions/index.ts
@@ -0,0 +1,14 @@
+import { ADD_TODO, TOGGLE_TODO, CHANGE_VISIBILITY } from "../constant";
+
+// 接收用户输入的待办事项，组装成 add_todo creator，向待办列表中添加新的待办事项
+// 单个 todo 的数据格式：{id, todo, completed}
+export const add_todo = (TodoObj) => ({ type: ADD_TODO, data: TodoObj });
+
+// 接收id，通过对比待办事项的id，实现完成状态的切换
+export const toggle_todo = (id) => ({ type: TOGGLE_TODO, data: id });
+
+// 接收filter，实现待办事项的可见性
+export const change_visibility = (filter) => ({
+  type: CHANGE_VISIBILITY,
+  filter,
+});
