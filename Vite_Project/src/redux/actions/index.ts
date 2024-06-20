b73c46310e7ba889512f29300cf2c6e81599ba47
syh <807968172@qq.com>
2024-06-20 09:31:41 +0800

    大作业actions的主文件，主要包含add_todo和toggle_todo的动作包装器，add_todo接收待办事项对象，toggle_todo接收待办id

--- Vite_Project/src/redux/actions/index.ts
+++ Vite_Project/src/redux/actions/index.ts
@@ -4,5 +4,5 @@ import { ADD_TODO, TOGGLE_TODO, CHANGE_VISIBILITY } from "../constant";
 // 单个 todo 的数据格式：{id, todo, completed}
 export const add_todo = (TodoObj) => ({ type: ADD_TODO, data: TodoObj });
 
-// 接收id，通过对比待办事项的id，实现完成状态的切换
+// 接收id，通过对比待办事项的id，实现完成状态的变化
 export const toggle_todo = (id) => ({ type: TOGGLE_TODO, data: id });
