a793130aafd274a7724792472fae0384c257082f
syh <807968172@qq.com>
2024-06-21 10:38:11 +0800

    简化Content容器的实现，将路由表和TodoList组件分离

--- Vite_Project/src/containers/Content/index.tsx
+++ Vite_Project/src/containers/Content/index.tsx
@@ -1,30 +1,33 @@
 import React from "react";
 import { connect } from "react-redux";
 import { toggle_todo } from "../../redux/actions";
+import { useRoutes } from "react-router-dom";
+import createRoutes from "../../routes";
+import MyNavLink from "../../components/MyNavLink";
 
 function Content({ todos, toggle_todo }) {
   const handleChecked = (id) => {
     toggle_todo(id);
   };
 
+  const present = todos.present ? todos.present : [];
+  const allTodos = todos;
+  const completedTodos = present.filter((todo) => todo.completed);
+  const activeTodos = present.filter((todo) => !todo.completed);
+
+  const routes = useRoutes(
+    createRoutes({ allTodos, completedTodos, activeTodos, handleChecked })
+  );
+
   return (
     <div>
-      <ul style={{ listStyle: "none" }}>
-        {todos.map((todo) => {
-          return (
-            <li key={todo.id}>
-              <label>
-                <input
-                  type="checkbox"
-                  checked={todo.completed}
-                  onChange={() => handleChecked(todo.id)}
-                />
-                <span>{todo.todo}</span>
-              </label>
-            </li>
-          );
-        })}
-      </ul>
+      {/* todo: 这里可以用路由实现 */}
+      <MyNavLink to="/all">全部</MyNavLink>
+      &nbsp;
+      <MyNavLink to="/completed">已完成</MyNavLink>
+      &nbsp;
+      <MyNavLink to="/active">进行中</MyNavLink>
+      {routes}
     </div>
   );
 }
